ba44d678b944113016b789ecdf2c6dfa8586fa42
Night-Shift101 <24gavinfox@gmail.com>
2025-06-04 09:43:13 -0400

    Initializes game environment
    Sets up the initial game environment by creating instances of the core game classes.
    
    It initializes the player, enemy, and floor, preparing the groundwork for further game logic.

---
+++ classes/floor_manager.py
@@ -0,0 +1,4 @@
+class Floor:
+    def __init__(self, floor_number: int):
+        self.floor_number = floor_number
+        
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print("Hello World!")
-print("test")
-print("Test2")
+from classes.enemy_manager import Enemy
+from classes.player_manager import Player
+from classes.floor_manager import Floor
+
+enemy = Enemy("Goblin")
+player = Player("Hero")
+floor = Floor(1)
+print(floor.floor_number, player.name, enemy.name)
